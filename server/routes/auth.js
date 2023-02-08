26843e21572cdd0a79354d741029f3b1eca4cd14
rodgersmerokaosumo <rodgersmerokaosumo@gmail.com>
2023-02-08 12:43:30 +0300

    signin pause 2:47

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/user.js");
 const authRouter = express.Router();
 const bcryptjs = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 
 
 //signup
@@ -30,5 +31,30 @@ authRouter.post("/api/signup", async (req, res) =>{
         res.status(500).json({error: e.message})
     }
 })
+
+//sign in route
+authRouter.post('/api/signin', async(req, res)=>{
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({msg: "User with this email does not exist"});
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({msg: "Incorrect password"});
+        }
+
+        jwt.sign({id: user._id}, "passwordkey");
+        res.json({token, ...user._doc})
+    } catch (e) {
+        res.status(500).json({error: e.message})
+    }
+})
+
 
 module.exports = authRouter;
